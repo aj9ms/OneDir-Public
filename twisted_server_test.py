986be95355ed062b79c263a770579c2e82c93621
benedgar <bde2wd@virginia.edu>
2014-03-25 14:38:20 -0400

    The twisted_server_test file now is a working server that you can 'login' to

--- twisted_server_test.py
+++ twisted_server_test.py
@@ -4,30 +4,45 @@ from twisted.internet.protocol import Factory
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet import reactor
 
-class Echo(Protocol):
+class Auth(Protocol):
 
     def __init__(self, factory):
         self.factory = factory
+        self.state = "initialize"
+        self.username = ""
+        self.password = ""
 
     def connectionMade(self):
         self.factory.numProtocols = self.factory.numProtocols+1
         self.transport.write(
             "Welcome! There are currently %d open connections.\n" %
             (self.factory.numProtocols,))
+        self.transport.write("Please Login.\nUsername: ")
+        self.state = "username"
 
     def connectionLost(self, reason):
         self.factory.numProtocols = self.factory.numProtocols-1
 
     def dataReceived(self, data):
-        self.transport.write(data)
+        if self.state == "username":
+            self.username = data.strip()
+            self.transport.write("Password: ")
+            self.state = "password"
+        elif self.state == "password":
+            self.password = data.strip()
+            self.state = "loggedin"
+        elif data.strip() == "quit":
+            self.transport.loseConnection()
+        else:
+            self.transport.write("Welcome " + self.username + "!\n")
 
-class QOTDFactory(Factory):
+class AuthFactory(Factory):
     def __init__(self):
         self.numProtocols = 0
     def buildProtocol(self, addr):
-        return Echo(self)
+        return Auth(self)
 
 # 8007 is the port you want to run under. Choose something >1024
 endpoint = TCP4ServerEndpoint(reactor, 8007)
-endpoint.listen(QOTDFactory())
+endpoint.listen(AuthFactory())
 reactor.run()
