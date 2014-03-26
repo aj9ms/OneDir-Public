a2fb76cacdf19a7f0f6bc405338c4534b9327942
aj9ms <aj9ms@virginia.edu>
2014-03-25 22:50:59 -0400

    added some event handling changes
    notifies user when directory has a file that's modified, created, or when directory is added.

--- watchdog_test.py
+++ watchdog_test.py
@@ -1,10 +1,24 @@
-# A simple program that uses watchdog to monitor directories specified as
-# command-line arguments and logs events generated
 import sys
 import time
 import logging
+from watchdog.events import LoggingEventHandler, FileSystemEventHandler
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+#Thread-safe event queue that skips adding same events consecutively.
+#Has single queue_event() method
+from watchdog.observers.api import EventQueue
+from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        print "file modified!"
+    def on_created(self, event):
+        print "file created!"
+    def on_moved(self, event):
+	print "file moved!"
+    def on_any_event(self, event):
+        if event.is_directory:
+	     print "directory changed"
+        print event.src_path
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
@@ -12,14 +26,17 @@ if __name__ == "__main__":
                         datefmt='%Y-%m-%d %H:%M:%S')
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
     # This event handler inherits from FileSystemEventHandler
-    #   Which has methods to implement like on_any_event(FileSystemEvent)
-    #   on_modified(event), on_moved(event), etc...
-    #   Check out this page -- https://pythonhosted.org/watchdog/api.html
+    # Which has methods to implement like on_any_event(FileSystemEvent)
+    # on_modified(event), on_moved(event), etc...
+    # Check out this page -- https://pythonhosted.org/watchdog/api.html
 
     # Events have fields like -- event_type, is_directory, src_path,
-    event_handler = LoggingEventHandler()
+    #event_handler = LoggingEventHandler()
+    event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
+    #not entirely sure how walker_callback works
+    #snap = DirectorySnapshot(path, recursive=True, walker_callback=(lambda p, s: None))
     observer.start()
     try:
         while True:
