990b91a228a6afc16399bd4e2ad7ab4cb350949c
L33t Hax0r <aj9ms@virginia.edu>
2014-04-13 13:08:46 -0400

    Temporary files do not show up in user.log file anymore

--- watchdog_test.py
+++ watchdog_test.py
@@ -1,7 +1,7 @@
 import sys
 import time
 import logging
-from watchdog.events import LoggingEventHandler, FileSystemEventHandler, FileSystemEvent
+from watchdog.events import FileSystemEventHandler, PatternMatchingEventHandler
 from watchdog.observers import Observer
 #Thread-safe event queue that skips adding same events consecutively.
 #Has single queue_event() method
@@ -9,57 +9,54 @@ from watchdog.observers.api import EventQueue
 from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
 
 class MyHandler(FileSystemEventHandler):
+#class MyHandler(PatternMatchingEventHandler):
+    #def __init__(self, patterns=None, ignore_patterns=None, ignore_directories=False, case_sensitive=False):
+        #saves all changes made in user.log
+        #super(MyHandler, self).__init__(patterns, ignore_patterns, ignore_directories, case_sensitive)
     def __init__(self):
         logging.basicConfig(filename='user.log', level=logging.INFO,
                             format='%(asctime)s - %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S')
     def on_modified(self, event):
-        """if event.is_directory:
-            print "directory modified"
-        else:
-            print "file modified!"""
-        logging.warning(event.src_path + ' modified')
+        source = event.src_path
+        directorylist = source.split('/')
+        last = directorylist[len(directorylist) - 1]
+        if not last.startswith('.goutputstream'):
+            logging.warning(event.src_path + ' modified')
     def on_created(self, event):
-       """ if event.is_directory:
-            print "directory created"
-        else:
-            print "file created!"""
-       logging.warning(event.src_path + ' created')
+        source = event.src_path
+        source_tilde = source[len(source)-1]
+        directorylist = source.split('/')
+        last = directorylist[len(directorylist) - 1]
+        if not last.startswith('.goutputstream'):
+            if source_tilde == "~":
+                logging.warning(source[0:len(source)-1] + ' created')
+            else:
+                logging.warning(event.src_path + ' created')
     def on_moved(self, event):
-        """if event.is_directory:
-            print "directory moved"
-        else:
-            print "file moved!"""
-        logging.warning(event.src_path + ' moved')
+        source = event.src_path
+        directorylist = source.split('/')
+        last = directorylist[len(directorylist) - 1]
+        if not last.startswith('.goutputstream'):
+            logging.warning(event.src_path + ' moved')
     def on_deleted(self, event):
-        """if event.is_directory:
-            print "directory deleted"
+        source = event.src_path
+        source_tilde = source[len(source)-1]
+        if source_tilde == "~":
+            logging.warning(source[0:len(source)-1] + ' deleted')
         else:
-            print "file deleted"""
-        logging.warning(event.src_path + ' deleted')
+            logging.warning(event.src_path + ' deleted')
     def on_any_event(self, event):
         source = event.src_path
-        #directory = source.split("/")
         print source[source.find("/OneDir/", 0, len(source))+8:]
 
-def walker_callback(path, stat_info, self=None):
-    return
-
-#class SnapshotMonitor():
-#    def scan(self):        
-
 if __name__ == "__main__":
-#    logging.basicConfig(level=logging.INFO,
-#                        format='%(asctime)s - %(message)s',
-#                        datefmt='%Y-%m-%d %H:%M:%S')
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
     event_handler = MyHandler()
     logging.warning('watchdog started')
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
-    #stringtest = "/OneDir/Test Folder/test.txt"
-    #print stringtest[stringtest.find("OneDir", 0, len(stringtest))+6:]
     try:
         while True:
             time.sleep(1)
