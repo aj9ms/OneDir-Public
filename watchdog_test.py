02ad7b645eac116b8a7cdea3997236fe9117cd37
L33t Hax0r <aj9ms@virginia.edu>
2014-04-13 15:00:58 -0400

    Updated with comments (all copied over to client.py, but kept here as back-up)

--- watchdog_test.py
+++ watchdog_test.py
@@ -9,55 +9,76 @@ from watchdog.observers.api import EventQueue
 from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
 
 class MyHandler(FileSystemEventHandler):
-#class MyHandler(PatternMatchingEventHandler):
-    #def __init__(self, patterns=None, ignore_patterns=None, ignore_directories=False, case_sensitive=False):
-        #saves all changes made in user.log
-        #super(MyHandler, self).__init__(patterns, ignore_patterns, ignore_directories, case_sensitive)
     def __init__(self):
         logging.basicConfig(filename='user.log', level=logging.INFO,
                             format='%(asctime)s - %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S')
-    def on_modified(self, event):
+    #on_modified doesn't get called unless we create a new file
+    #useless because on_created gets called when creating new file anyway
+    """def on_modified(self, event):
         source = event.src_path
         directorylist = source.split('/')
         last = directorylist[len(directorylist) - 1]
         if not last.startswith('.goutputstream'):
             logging.warning(event.src_path + ' modified')
-            print source[source.find("/OneDir/", 0, len(source))+8:] + ' modified'
+            print source[source.find("/OneDir/", 0, len(source))+8:] + ' modified'"""
+    #creates both files and directories
+    #for directories, the created directory will ALWAYS first be called "Untitled Folder"
+    #renaming folders comes in on_moved
     def on_created(self, event):
-        source = event.src_path
-        source_tilde = source[len(source)-1]
-        directorylist = source.split('/')
-        last = directorylist[len(directorylist) - 1]
-        if not last.startswith('.goutputstream'):
-            if source_tilde == "~":
-                logging.warning(source[0:len(source)-1] + ' created')
-                print source[source.find("/OneDir/", 0, len(source))+8:len(source)-1] + ' created'
-            else:
-                logging.warning(event.src_path + ' created')
-                print source[source.find("/OneDir/", 0, len(source))+8:] + ' created'
+        #creating directory
+        if event.is_directory:
+            source = event.src_path
+            source_tilde = source[len(source)-1]
+            directorylist = source.split('/')
+            last = directorylist[len(directorylist) - 1]
+            if not last.startswith('.goutputstream'):
+                if source_tilde == "~":
+                    logging.warning(source[0:len(source)-1] + ' created')
+                    print source[source.find("/OneDir/", 0, len(source))+8:len(source)-1] + ' created'
+                else:
+                    logging.warning(event.src_path + ' created')
+                    print source[source.find("/OneDir/", 0, len(source))+8:] + ' created'
+        #creating a file
+        else:
+            source = event.src_path
+            source_tilde = source[len(source)-1]
+            directorylist = source.split('/')
+            last = directorylist[len(directorylist) - 1]
+            if not last.startswith('.goutputstream'):
+                if source_tilde == "~":
+                    logging.warning(source[0:len(source)-1] + ' created')
+                    print source[source.find("/OneDir/", 0, len(source))+8:len(source)-1] + ' created'
+                else:
+                    logging.warning(event.src_path + ' created')
+                    print source[source.find("/OneDir/", 0, len(source))+8:] + ' created'
+    #moving files
+    #renaming a directory
+    #moving a directory
     def on_moved(self, event):
         source = event.src_path
         directorylist = source.split('/')
         last = directorylist[len(directorylist) - 1]
         dest = event.dest_path
         destlist = dest.split('/')
+        #not a temp file but is still a file (file moving)
         if not last.startswith('.goutputstream') and not event.is_directory:
-            #not a temp file and not a directory
+            #not a temp file but is still a file
             logging.warning(event.src_path + ' movedto ' + event.dest_path)
             print source[source.find("/OneDir/", 0, len(source))+8:] + ' movedto ' + dest[dest.find("/OneDir/", 0, len(dest))+8:]
+        #event is a directory (directory moving AND renaming)
         elif event.is_directory:
-            #event is directory (so can't be temp file)
             sourcepath = source[0:source.find(directorylist[len(directorylist)-1])]
             destpath = dest[0:dest.find(destlist[len(destlist)-1])]
+            #rename directory
             if sourcepath == destpath:
-                #rename
                 print source[source.find("/OneDir/", 0, len(source))+8:] + ' renamedto ' + dest[dest.find("/OneDir/", 0, len(dest))+8:]
                 logging.warning(event.src_path + ' renamedto ' + event.dest_path)
+            #move directory
             else:
-                #moved directory
                 print source[source.find("/OneDir/", 0, len(source))+8:] + ' movedto ' + dest[dest.find("/OneDir/", 0, len(dest))+8:]
                 logging.warning(event.src_path + ' movedto ' + event.dest_path)
+    #deleting files and folders (when deleting a folder, it lists all files to delete as well)
     def on_deleted(self, event):
         source = event.src_path
         source_tilde = source[len(source)-1]
